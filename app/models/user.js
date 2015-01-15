b3d06218f8e5ee75dd6d2c068d2f128987adddb9
Hack Reactor Students <accounts@hackreactor.com>
2015-01-14 20:02:40 -0800

    Committing first changes

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,22 @@
-var db = require('../config');
+var mongoose = require('mongoose');
+var user = require('../config').user;
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
+user.methods.comparePassword = function (pw, cb) {
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+user.methods.hashPassword = function () {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
     });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});
+};
+
+var User = mongoose.model('User', user);
 
 module.exports = User;
